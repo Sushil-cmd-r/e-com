04572d14bdb3c702719c4db0b6d83adf23cc0e40
Sushil-cmd-r <kandharesushil10@gmail.com>
2022-10-27 15:19:23 +0530

    Navbar Done

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,38 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Menu, Close } from '../../assets/svgs'
 import './navbar.scss'
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState('false')
+
   return (
-    <nav className='nav'>Navbar</nav>
+    <nav className='nav'>
+      <div className="nav-left">
+        <Link to='/' className='link'>
+          LOGO
+        </Link>
+      </div>
+      <div className="nav-right">
+        <span className='nav-toggle' onClick={() => setToggleMenu(prev => !prev)}>
+          {toggleMenu ? <Menu /> : <Close />}
+        </span>
+        <div className={`nav-links ${!toggleMenu ? 'active' : ''}`}>
+          <div className="nav-link">
+            <a href='#products' className='link'>Explore</a>
+          </div>
+          <div className="nav-link">
+            <Link to='/list' className='link'>Create</Link>
+          </div>
+          <div className="nav-link">
+            <Link to='/list' className='link'>Log In</Link>
+          </div>
+          <div className="nav-link">
+            <Link to='/list' className='link'>Register</Link>
+          </div>
+        </div>
+      </div>
+    </nav >
   )
 }
 
